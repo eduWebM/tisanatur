1093a5b42de807c9354cbcc5c6cdc2ed9c8d13d5
eduWebM <147324725+eduWebM@users.noreply.github.com>
2024-08-15 11:58:47 +0200

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -27,8 +27,8 @@ d.addEventListener("DOMContentLoaded", async(e) => {
     }
 
     buscador("search", "#busqueda", ".contain-search-close", "#busqueda-template", ".section-busqueda");
-    console.log(path);
-    if (path === '/' || path === '/index.html' || path === '/categoria.html') {
+
+    if (path === '/' || path === '/index.html' || path === '/categoria.html' || path === '/tisanatur/' || path === '/tisanatur/index.html') {
         cargarPosts(".latest", "#post-template", ".fondo", "#next", "#prev");
         popular(".popular", ".list-popular");
     } else {
